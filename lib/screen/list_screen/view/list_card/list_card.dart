ac7983b848a9cf10a165083276fbb2fac0bc7372
hironobu.iga <hironobu.iga.developer@gmail.com>
2021-11-28 15:32:29 +0900

    feat: implement list screen card

--- lib/screen/list_screen/view/list_card/list_card.dart
+++ lib/screen/list_screen/view/list_card/list_card.dart
@@ -1,10 +1,40 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter/material.dart';
+import 'package:graphql_sample_kaigi_2021/network/mutation/add_star/add_star_mutation_provider.dart';
+import 'package:graphql_sample_kaigi_2021/network/mutation/remove_star/remove_star_mutation_provider.dart';
+import 'package:graphql_sample_kaigi_2021/screen/list_screen/view/list_card/graphql/list_card.fragment.data.gql.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class ListCard extends ConsumerWidget {
+class ListCard extends HookConsumerWidget {
+  const ListCard(this.listCardFragment);
+
+  final GListCardFragmentData listCardFragment;
+
   @override
-  Widget build(BuildContext context, ScopedReader watch) {
-    // TODO: implement build
-    throw UnimplementedError();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final addStarMutation = ref.watch(addStarMutationProvider);
+    final removeStarMutation = ref.watch(removeStarMutationProvider);
+
+    return Column(
+      children: [
+        Row(children: [
+          Text(listCardFragment.name),
+          InkWell(
+            onTap: () => listCardFragment.viewerHasStarred ?
+            removeStarMutation.removeStar(id: listCardFragment.id) :
+            addStarMutation.addStar(id: listCardFragment.id)
+            ,
+            child: Icon(
+              listCardFragment.viewerHasStarred ?
+              Icons.favorite :
+              Icons.favorite_border,
+            ),
+          ),
+        ],),
+        Text(
+          listCardFragment.description ?? "",
+          maxLines: 2,
+        ),
+      ],
+    );
   }
 }
